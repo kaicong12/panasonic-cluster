a5df0f881ef4323a708cbeb207aaae65aee3b48b
Haoyu <haoyusimon927@gmail.com>
2022-08-01 16:14:41 +0800

    Regenerated data table

--- gen_data_table/gen_data_table.py
+++ gen_data_table/gen_data_table.py
@@ -1,7 +1,19 @@
 import prettytable
 import csv
 import os
-
+qp_sets = {
+    "FLIR": "QP0",
+    "OpenImages": "QP0",
+    "TVD_image": "QP0",
+    "SFU_A": "QP1",
+    "SFU_B": "QP2",
+    "SFU_C": "QP3",
+    "SFU_D": "QP0",
+    "SFU_E": "QP0",
+    "TVD-01" : "QP0",
+    "TVD-02" : "QP4",
+    "TVD-03" : "QP0",
+}
 id = 0
 
 image_data = prettytable.PrettyTable()
@@ -17,7 +29,7 @@ with open('flir.csv') as csv_file:
     for row in csv_reader:
         name = row[0]
         print(name)
-        image_data.add_row([id, name, "QP0", "FLIR"])
+        image_data.add_row([id, name, qp_sets["FLIR"], "FLIR"])
         id += 1
 
 
@@ -26,7 +38,7 @@ with open('openImage.csv') as csv_file:
     for row in csv_reader:
         name = row[0].split("/")[1]
         print(name)
-        image_data.add_row([id, name, "QP1", "openImage"])
+        image_data.add_row([id, name, qp_sets["OpenImages"], "OpenImages"])
         id += 1
 
 with open('tvd_image.csv') as csv_file:
@@ -34,7 +46,7 @@ with open('tvd_image.csv') as csv_file:
     for row in csv_reader:
         name = row[0]
         print(name)
-        image_data.add_row([id, name, "QP2", "TVD_image"])
+        image_data.add_row([id, name, qp_sets["TVD_image"], "TVD_image"])
         id += 1
 
 image_data.align = "l"
@@ -51,38 +63,38 @@ TVD_video_names = ["TVD-01", "TVD-02", "TVD-03"]
 SFU_video_names = ["Traffic_2560x1600_30_crop", "Kimono1_1920x1080_24", "ParkScene_1920x1080_24", "Cactus_1920x1080_50", "BasketballDrive_1920x1080_50", "BQTerrace_1920x1080_60", "BasketballDrill_832x480_50", "BQMall_832x480_60", "PartyScene_832x480_50", "RaceHorses_832x480_30", "BasketballPass_416x240_50", "BQSquare_416x240_60", "BlowingBubbles_416x240_50", "RaceHorses_416x240_30", "FourPeople_1280x720_60", "Johnny_1280x720_60", "KristenAndSara_1280x720_60"]
 
 tvd_dict = { # (IntraPeriod, FrameRate, FramesToBeEncoded, FrameSkip, QP_set_id)
-    "TVD-01"                       : (64, 50, 3000, 0, "QP3"),
-    "TVD-02"                       : (64, 50, 636, 0, "QP4"),
-    "TVD-03"                       : (64, 50, 2334, 0, "QP3"),
+    "TVD-01"                       : (64, 50, 3000, 0),
+    "TVD-02"                       : (64, 50, 636, 0),
+    "TVD-03"                       : (64, 50, 2334, 0),
 }
 
-sfu_dict = { # (IntraPeriod, FrameRate, FramesToBeEncoded, FrameSkip, QP_set_id)
-    "Traffic_2560x1600_30_crop"    : (32, 30, 33, 117, "QP5"),
-    "Kimono1_1920x1080_24"         : (32, 24, 33, 207, "QP6"),
-    "ParkScene_1920x1080_24"       : (32, 24, 33, 207, "QP6"),
-    "Cactus_1920x1080_50"          : (64, 50, 97, 403, "QP6"),
-    "BQTerrace_1920x1080_60"       : (64, 60, 129, 471, "QP6"),
-    "BasketballDrive_1920x1080_50" : (64, 50, 97, 403, "QP6"),
-    "BQMall_832x480_60"            : (64, 60, 129, 471, "QP7"),
-    "BasketballDrill_832x480_50"   : (64, 50, 97, 403, "QP7"),
-    "PartyScene_832x480_50"        : (64, 50, 97, 403, "QP7"),
-    "RaceHorses_832x480_30"        : (32, 30, 65, 235, "QP7"),
-    "BQSquare_416x240_60"          : (64, 60, 129, 471, "QP8"),
-    "BasketballPass_416x240_50"    : (64, 50, 97, 403, "QP8"),
-    "BlowingBubbles_416x240_50"    : (64, 50, 97, 403, "QP8"),
-    "RaceHorses_416x240_30"        : (32, 30, 65, 235, "QP8"),
-    "KristenAndSara_1280x720_60"   : (64, 60, 129, 471, "QP9"),
-    "Johnny_1280x720_60"           : (64, 60, 129, 471, "QP9"),
-    "FourPeople_1280x720_60"       : (64, 60, 129, 471, "QP9"),
+sfu_dict = { # (IntraPeriod, FrameRate, FramesToBeEncoded, FrameSkip, QP_set)
+    "Traffic_2560x1600_30_crop"    : (32, 30, 33, 117, "SFU_A"),
+    "Kimono1_1920x1080_24"         : (32, 24, 33, 207, "SFU_B"),
+    "ParkScene_1920x1080_24"       : (32, 24, 33, 207, "SFU_B"),
+    "Cactus_1920x1080_50"          : (64, 50, 97, 403, "SFU_B"),
+    "BQTerrace_1920x1080_60"       : (64, 60, 129, 471, "SFU_B"),
+    "BasketballDrive_1920x1080_50" : (64, 50, 97, 403, "SFU_B"),
+    "BQMall_832x480_60"            : (64, 60, 129, 471, "SFU_C"),
+    "BasketballDrill_832x480_50"   : (64, 50, 97, 403, "SFU_C"),
+    "PartyScene_832x480_50"        : (64, 50, 97, 403, "SFU_C"),
+    "RaceHorses_832x480_30"        : (32, 30, 65, 235, "SFU_C"),
+    "BQSquare_416x240_60"          : (64, 60, 129, 471, "SFU_D"),
+    "BasketballPass_416x240_50"    : (64, 50, 97, 403, "SFU_D"),
+    "BlowingBubbles_416x240_50"    : (64, 50, 97, 403, "SFU_D"),
+    "RaceHorses_416x240_30"        : (32, 30, 65, 235, "SFU_D"),
+    "KristenAndSara_1280x720_60"   : (64, 60, 129, 471, "SFU_E"),
+    "Johnny_1280x720_60"           : (64, 60, 129, 471, "SFU_E"),
+    "FourPeople_1280x720_60"       : (64, 60, 129, 471, "SFU_E"),
 }
 
 for name in TVD_video_names:
-    video_data.add_row([id, name, tvd_dict[name][4], tvd_dict[name][0], tvd_dict[name][1], tvd_dict[name][2], tvd_dict[name][3], "TVD_video"])
+    video_data.add_row([id, name, qp_sets[name], tvd_dict[name][0], tvd_dict[name][1], tvd_dict[name][2], tvd_dict[name][3], "TVD_video"])
     id += 1
 
 
 for name in SFU_video_names:
-    video_data.add_row([id, name, sfu_dict[name][4], sfu_dict[name][0], sfu_dict[name][1], sfu_dict[name][2], sfu_dict[name][3], "SFU_HW"])
+    video_data.add_row([id, name, qp_sets[sfu_dict[name][4]], sfu_dict[name][0], sfu_dict[name][1], sfu_dict[name][2], sfu_dict[name][3], "SFU_HW"])
     id += 1
 video_data.align = "l"
 # print(type(image_data))
